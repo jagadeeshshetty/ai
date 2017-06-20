69d350512928a168f00a56943be878f8a1a41f8c
Jagadeesh <jagadeesh.cheluvaraj@gmail.com>
2017-06-20 07:45:03 +0530

    Heron's algorithm update

---
+++ scripts/Heron.py
@@ -0,0 +1,10 @@
+def findSqrt(N, guessFactor, flag):
+    squareRoot = (N/guessFactor)
+    average = ((squareRoot+guessFactor)/2)
+
+    if flag == True:
+        findSqrt(N, average, False)
+    else:
+        print(average)
+
+findSqrt(500, 20, True)
