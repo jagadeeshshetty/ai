88fa2dc2d44f33fc98b8edb72b44e55970aaaab1
Jagadeesh <jagadeesh.cheluvaraj@gmail.com>
2017-06-26 16:53:22 +0530

    Windows PyCharm commit for AL.

---
+++ scripts/al/isPhoneNumber.py
@@ -0,0 +1,10 @@
+
+def isPhoneNumber(text):
+    if len(text) != 12:
+        return False
+    for i in range(0, 3):
+        if not text[i].isdecimal():
+            return False
+    
+
+    return True
